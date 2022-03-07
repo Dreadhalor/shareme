451efa95c5be11ff09c031d6c16bfeaeacb208b1
Dreadhalor <Scotthetrick2@yahoo.com>
2022-03-07 00:48:05 -0800

    Starting on the Login & Home components; added some assets to use for those but it feels odd putting assets in a git repository so I added the assets folder to gitignore

--- shareme-frontend/src/App.tsx
+++ shareme-frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './containers/Home';
+
 const App = (): JSX.Element => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+  );
+  // <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 };
 
 export default App;

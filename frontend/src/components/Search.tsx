0c6ef3e9db7e7c60eca8a622d88837070cb49144
Dreadhalor <Scotthetrick2@yahoo.com>
2022-04-11 23:33:20 -0700

    Favicons are properly set up (MacOS Safari has cache issues but hopefully the icon is the right color) Ready to deploy!

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,5 +1,38 @@
-const Search = (props: any) => {
-  return <div>Search</div>;
+import { useEffect, useState } from 'react';
+import { MasonryLayout, Spinner } from 'components';
+import { client } from 'client';
+import { feedQuery, searchQuery } from 'utils/data';
+
+const Search = ({ searchTerm, setSearchTerm }: any) => {
+  const [pins, setPins] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {/* {searchTerm} */}
+      {loading ? (
+        <Spinner message='Searching for pins...' />
+      ) : (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  );
 };
 
 export default Search;

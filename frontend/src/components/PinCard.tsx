b437d89e8205e0e2aa6954ab73cdcd4041fec792
Dreadhalor <Scotthetrick2@yahoo.com>
2022-04-20 01:18:50 -0700

    Added a check to debounce toggling saving a pin

--- frontend/src/components/PinCard.tsx
+++ frontend/src/components/PinCard.tsx
@@ -60,75 +60,80 @@ const PinCard = ({ pin }: { pin: IPin }) => {
             src={urlFor(image).width(250).url()}
           />
         )}
-        {!editing && (
-          <>
-            {postHovered && (
-              <div className='absolute top-0 z-50 flex h-full w-full flex-col justify-between p-1.5'>
-                <div className='flex items-center justify-between'>
-                  <div className='flex flex-row gap-2'>
-                    <a
-                      href={`${image?.asset?.url}?dl=`}
-                      download
-                      onClick={(e) => e.stopPropagation()}
-                      // again no clue why outline-none is here but I don't care enough to question this anymore
-                      className='text-dark flex h-9 w-9 items-center justify-center rounded-full bg-white text-xl opacity-75 outline-none hover:opacity-100 hover:shadow-md'
-                    >
-                      <MdDownloadForOffline />
-                    </a>
-                  </div>
-                  {alreadySaved ? (
-                    <button
-                      type='button'
-                      className='rounded-3xl bg-blue-500 px-5 py-1 text-base font-bold text-white opacity-70 outline-none hover:opacity-100 hover:shadow-md'
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        toggleSave(_id);
-                      }}
-                    >
-                      {save_length} saved!
-                    </button>
-                  ) : (
-                    <button
-                      type='button'
-                      className='rounded-3xl bg-red-500 px-5 py-1 text-base font-bold text-white opacity-70 outline-none hover:opacity-100 hover:shadow-md'
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        toggleSave(_id);
-                      }}
-                    >
-                      {save_length} saved
-                    </button>
-                  )}
-                </div>
-                <div className='flex w-full items-center justify-between gap-2'>
-                  {destination && (
-                    <a
-                      href={destination}
-                      target='blank'
-                      rel='noreferrer'
-                      className='flex items-center gap-2 truncate rounded-full bg-white py-2 px-4 text-right font-bold text-black opacity-70 hover:opacity-100 hover:shadow-md'
-                      onClick={(e) => e.stopPropagation()}
-                    >
-                      <BsFillArrowUpRightCircleFill className='flex-shrink-0' />
-                      <span className='truncate'>{cleanUrl(destination)}</span>
-                    </a>
-                  )}
-                  {postedBy?._id === user?.googleId && (
-                    <button
-                      type='button'
-                      className='text-dark rounded-3xl bg-white p-2 text-base font-bold opacity-70 outline-none hover:opacity-100 hover:shadow-md'
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        deletePin(_id);
-                      }}
-                    >
-                      <AiTwotoneDelete />
-                    </button>
-                  )}
-                </div>
+
+        {postHovered && (
+          <div className='absolute top-0 z-50 flex h-full w-full flex-col justify-between p-1.5'>
+            <div className='flex items-center justify-between'>
+              <div className='flex flex-row gap-2'>
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  // again no clue why outline-none is here but I don't care enough to question this anymore
+                  className='text-dark flex h-9 w-9 items-center justify-center rounded-full bg-white text-xl opacity-75 outline-none hover:opacity-100 hover:shadow-md'
+                >
+                  <MdDownloadForOffline />
+                </a>
               </div>
-            )}
-          </>
+              {editing ? (
+                <button
+                  type='button'
+                  className='rounded-3xl bg-blue-500 px-5 py-1 text-base font-bold text-white opacity-70 outline-none hover:opacity-100 hover:shadow-md'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  {alreadySaved ? 'Unsaving...' : 'Saving...'}
+                </button>
+              ) : alreadySaved ? (
+                <button
+                  type='button'
+                  className='rounded-3xl bg-blue-500 px-5 py-1 text-base font-bold text-white opacity-70 outline-none hover:opacity-100 hover:shadow-md'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    toggleSave(_id);
+                  }}
+                >
+                  {save_length} saved!
+                </button>
+              ) : (
+                <button
+                  type='button'
+                  className='rounded-3xl bg-red-500 px-5 py-1 text-base font-bold text-white opacity-70 outline-none hover:opacity-100 hover:shadow-md'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    toggleSave(_id);
+                  }}
+                >
+                  {save_length} saved
+                </button>
+              )}
+            </div>
+            <div className='flex w-full items-center justify-between gap-2'>
+              {destination && (
+                <a
+                  href={destination}
+                  target='blank'
+                  rel='noreferrer'
+                  className='flex items-center gap-2 truncate rounded-full bg-white py-2 px-4 text-right font-bold text-black opacity-70 hover:opacity-100 hover:shadow-md'
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <BsFillArrowUpRightCircleFill className='flex-shrink-0' />
+                  <span className='truncate'>{cleanUrl(destination)}</span>
+                </a>
+              )}
+              {postedBy?._id === user?.googleId && (
+                <button
+                  type='button'
+                  className='text-dark rounded-3xl bg-white p-2 text-base font-bold opacity-70 outline-none hover:opacity-100 hover:shadow-md'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
+            </div>
+          </div>
         )}
       </div>
       <Link
